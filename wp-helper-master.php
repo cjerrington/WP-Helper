d0e3155702a20c335cec4872095b8a52f762197a
cjerrington <cerringtondesigns@me.com>
2014-12-22 10:35:45 -0600

    Update wp-helper-master.php

--- wp-helper-master.php
+++ wp-helper-master.php
@@ -4,15 +4,18 @@ Plugin Name: WordPress Helper Master
 Plugin Script: wp-helper-master.php
 Plugin URI: https://github.com/cjerrington/WP-Helper/
 Description: Removes and speeds up WordPress. 
-Version: 1.3
-License: GPL
+Version: 1.4
 Author: Clayton Errington
 Author URI: http://claytonerrington.wix.com/portfolio
+License: GPL-2.0+
+License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+GitHub Plugin URI: https://github.com/cjerrington/WP-Helper
 
 === RELEASE NOTES ===
 2014-12-10 - v1.0 - first version
 2014-12-11 - v1.2 - add security, performance, and admin modifications
 2014-12-11 - v1.3 - add admin / options page
+2014-12-22 - v1.4 - add plugin specific function names
 */
 
 /*
@@ -33,6 +36,11 @@ Online: http://www.gnu.org/licenses/gpl.txt
 */
 
 // ------------------
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
 
 include_once(dirname(__FILE__).'/class.php');
 
