268b4fa6a9127e16d48c9d6e7827793678cc9c6b
cjerrington <cerringtondesigns@me.com>
2014-12-22 10:21:38 -0600

    change function names to be plugin specific

--- wp-helper-master.php
+++ wp-helper-master.php
@@ -38,105 +38,105 @@ include_once(dirname(__FILE__).'/class.php');
 
 // ++++++++++++++++ WP CLEANUP +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
  	
-if (get_option( 'wpt_cononical_links' )){remove_action ('wp_head', 'rsd_link');}
-if (get_option( 'wpt_manifest_file' )){remove_action( 'wp_head', 'wlwmanifest_link');}
-if (get_option( 'wpt_short_link' )){remove_action( 'wp_head', 'wp_shortlink_wp_head');}
-if (get_option( 'wpt_wp_generator' )){remove_action( 'wp_head', 'wp_generator');}
-if (get_option( 'wpt_adjacent_posts' )){
-	remove_action( 'wpt_wp_head', 'adjacent_posts_rel_link_wp_head' );
-	remove_action( 'wpt_wp_head', 'adjacent_posts_rel_link');
+if (get_option( 'wphm_cononical_links' )){remove_action ('wp_head', 'rsd_link');}
+if (get_option( 'wphm_manifest_file' )){remove_action( 'wp_head', 'wlwmanifest_link');}
+if (get_option( 'wphm_short_link' )){remove_action( 'wp_head', 'wp_shortlink_wp_head');}
+if (get_option( 'wphm_wp_generator' )){remove_action( 'wp_head', 'wp_generator');}
+if (get_option( 'wphm_adjacent_posts' )){
+	remove_action( 'wphm_wp_head', 'adjacent_posts_rel_link_wp_head' );
+	remove_action( 'wphm_wp_head', 'adjacent_posts_rel_link');
 }
-if (get_option( 'wpt_index_rel_link' )){remove_action( 'wp_head', 'index_rel_link' );}
-if (get_option( 'wpt_post_rel_link' )){
+if (get_option( 'wphm_index_rel_link' )){remove_action( 'wp_head', 'index_rel_link' );}
+if (get_option( 'wphm_post_rel_link' )){
 	remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 );
 	remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );
 }
-if (get_option( 'wpt_feed_links' )){
+if (get_option( 'wphm_feed_links' )){
 	remove_action( 'wp_head', 'feed_links_extra', 3 );
 	remove_action( 'wp_head', 'feed_links', 2 );
 }
-if (get_option( 'wpt_cononical_links' )){remove_action( 'wp_head', 'rel_canonical');}
+if (get_option( 'wphm_cononical_links' )){remove_action( 'wp_head', 'rel_canonical');}
 
-function wphelper_remove_version() {
+function wphm_wphelper_remove_version() {
 return '';
 }
-if (get_option( 'wpt_version' )){add_filter('the_generator', 'wphelper_remove_version');}
+if (get_option( 'wphm_version' )){add_filter('the_generator', 'wphm_wphelper_remove_version');}
 
 
-function im_disable_feed() {
-wp_die( __('No feed available,please visit our <a href="'. get_bloginfo('url') .'">homepage</a>!') );
+function wphm_disable_feeds() {
+wp_die( __('No feed available, please visit our <a href="'. get_bloginfo('url') .'">homepage</a>!') );
 }
-if (get_option( 'wpt_disable_feed' )){
-	add_action('do_feed', 'im_disable_feed', 1);
-	add_action('do_feed_rdf', 'im_disable_feed', 1);
-	add_action('do_feed_atom', 'im_disable_feed', 1);
-	add_action('do_feed_rss', 'im_disable_feed', 1);
-	add_action('do_feed_rss2', 'im_disable_feed', 1); 
+if (get_option( 'wphm_disable_feed' )){
+	add_action('do_feed', 'wphm_isable_feeds', 1);
+	add_action('do_feed_rdf', 'wphm_disable_feeds', 1);
+	add_action('do_feed_atom', 'wphm_disable_feeds', 1);
+	add_action('do_feed_rss', 'wphm_disable_feeds', 1);
+	add_action('do_feed_rss2', 'wphm_disable_feeds', 1); 
 }
 
 // ++++++++++++++++ PERFORMANCE +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
 if(extension_loaded("zlib") && (ini_get("output_handler") != "ob_gzhandler"))
-if(get_option( 'wpt_GZip' )){add_action('wp', create_function('', '@ob_end_clean();@ini_set("zlib.output_compression", 1);'));}
+if(get_option( 'wphm_GZip' )){add_action('wp', create_function('', '@ob_end_clean();@ini_set("zlib.output_compression", 1);'));}
 
-function im_remove_script_version( $src ){
+function wphm_remove_script_version( $src ){
 	$parts = explode( '?', $src );
 	return $parts[0];
 }
-if (get_option ( 'wpt_query_version' )){
-	add_filter( 'script_loader_src', 'im_remove_script_version', 15, 1 );
-	add_filter( 'style_loader_src', 'im_remove_script_version', 15, 1 );
+if (get_option ( 'wphm_query_version' )){
+	add_filter( 'script_loader_src', 'wphm_remove_script_version', 15, 1 );
+	add_filter( 'style_loader_src', 'wphm_remove_script_version', 15, 1 );
 }
 
-if (get_option( 'wpt_xmlrpc' )){remove_filter('atom_service_url','atom_service_url_filter');}
+if (get_option( 'wphm_xmlrpc' )){remove_filter('atom_service_url','atom_service_url_filter');}
 
-if(get_option ( 'wpt_autoformat' )){
+if(get_option ( 'wphm_autoformat' )){
 	remove_filter( 'the_content', 'wpautop' );
 	remove_filter( 'the_excerpt', 'wpautop' );
 }
 
 // ++++++++++++++++ SECURITY MODIFICATIONS ++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
 // Hide login form error messages
-if(get_option( 'wpt_login' )){add_filter('login_errors',create_function('$a', "return 'Error';"));}
+if(get_option( 'wphm_login' )){add_filter('login_errors',create_function('$a', "return 'Error';"));}
 
 // Remove WordPress version number
-function im_remove_wp_version() { return ''; }
-if(get_option( 'wpt_wp_version_number' )){add_filter('the_generator', 'im_remove_wp_version');}
+function wphm_remove_wp_version() { return ''; }
+if(get_option( 'wphm_wp_version_number' )){add_filter('the_generator', 'wphm_remove_wp_version');}
 
 // Disable XMLRPC
-if(get_option( 'wpt_disable_XMLRPC' )){add_filter('xmlrpc_enabled', '__return_false'); }
+if(get_option( 'wphm_disable_XMLRPC' )){add_filter('xmlrpc_enabled', '__return_false'); }
 
 // Set the maximum number of post revisions unless the constant is already set in wp-config.php
-if (get_option( 'wpt_post_revisions' )) {
-	$post_revisions = get_option( 'wpt_post_revisions' );
+if (get_option( 'wphm_post_revisions' )) {
+	$post_revisions = get_option( 'wphm_post_revisions' );
 	if (!defined('WP_POST_REVISIONS')) define('WP_POST_REVISIONS', $post_revisions);
 }
 
 // Disable Auto Linking of URLs in comments
-if (get_option( 'wpt_auto_linking' )){remove_filter('comment_text', 'make_clickable', 9);}
+if (get_option( 'wphm_auto_linking' )){remove_filter('comment_text', 'make_clickable', 9);}
 
 // Disable self-ping
-function no_self_ping( &$links ) {
+function wphm_no_self_ping( &$links ) {
 	$home = get_option( 'home' );
 	foreach ( $links as $l => $link )
 	if ( 0 === strpos( $link, $home ) )
 	unset($links[$l]);
 }
-if(get_option( 'wpt_self_ping' )){add_action( 'pre_ping', 'no_self_ping' ); }
+if(get_option( 'wphm_self_ping' )){add_action( 'pre_ping', 'wphm_no_self_ping' ); }
 
 //remove lost password link
-function remove_lostpassword_text ( $text ) {
+function wphm_remove_lostpassword_text ( $text ) {
      if ($text == 'Lost your password?'){$text = '';}
         return $text;
      }
-if(get_option( 'wpt_lost_pass' )){add_filter( 'gettext', 'remove_lostpassword_text' );}
+if(get_option( 'wphm_lost_pass' )){add_filter( 'gettext', 'wphm_remove_lostpassword_text' );}
 
 // ++++++++++++++++ ADMIN MODIFICATIONS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
 
 // Change the default WordPress greeting in Admin
-function im_replace_howdy( $wp_admin_bar ) {
+function wphm_replace_howdy( $wp_admin_bar ) {
 	$my_account=$wp_admin_bar->get_node('my-account');
-	if (get_option( 'wpt_greeting' )){
-		$title = get_option( 'wpt_greeting' );
+	if (get_option( 'wphm_greeting' )){
+		$title = get_option( 'wphm_greeting' );
 	}else{
 		$title = 'Welcome, ';
 	}
@@ -146,23 +146,23 @@ function im_replace_howdy( $wp_admin_bar ) {
 	'title' => $newtitle,
 	) );
 }
-if (get_option( 'wpt_greeting' )){add_filter( 'admin_bar_menu', 'im_replace_howdy', 25 );}
+if (get_option( 'wphm_greeting' )){add_filter( 'admin_bar_menu', 'wphm_replace_howdy', 25 );}
 
 // Add new Admin menu item "All Settings"
-function im_all_settings_link() {
+function wphm_all_settings_link() {
 	add_options_page(__('All Settings'), __('All Settings'), 'administrator', 'options.php');
 }
-if (get_option( 'wpt_all_settings' )){add_action('admin_menu', 'im_all_settings_link');}
+if (get_option( 'wphm_all_settings' )){add_action('admin_menu', 'wphm_all_settings_link');}
 
 
 // Add custom dashboard setting 
 add_action('wp_dashboard_setup', 'my_custom_dashboard_widgets');
 	function my_custom_dashboard_widgets() {
 	global $wp_meta_boxes; 
-	wp_add_dashboard_widget('custom_help_widget', 'WP Helper Support', 'custom_dashboard_help');
+	wp_add_dashboard_widget('custom_help_widget', 'WP Helper Support', 'wphm_custom_dashboard_help');
 }
 
-function custom_dashboard_help() {
+function wphm_custom_dashboard_help() {
 	echo '<p>Welcome to the Dashboard! Need help? Contact the developer at <a href="mailto:cerrigntondesigns@me.com">cerrigntondesigns@me.com</a>. 
 	<br />For more information visit: <a href="http://claytonerrington.wix.com/portfolio" target="_blank">His website</a></p>
 	<p>If you like this plugin, send him some money to keep it up. Buy him a coffee to show your support!</p>
@@ -185,6 +185,6 @@ function custom_dashboard_help() {
 
 }
 
-$settings = new WordPress_Plugin_Template_Settings( __FILE__ );
+$settings = new WP_Helper_Master_Settings( __FILE__ );
 
 ?>
